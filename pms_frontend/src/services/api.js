aadef3cfe3179e6426a0828fa370b45fc7bbd08f
Vivek Gengadiya <gengadiyavivek09@gmail.com>
2025-12-01 15:52:09 -0500

    deploy_frontend

--- pms_frontend/src/services/api.js
+++ pms_frontend/src/services/api.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = 'http://localhost:9000/api';
+const API_BASE_URL = `${import.meta.env.VITE_API_URL}/api`;
+
 
 // Generic API call function with auth
 export const apiCall = async (endpoint, options = {}) => {
