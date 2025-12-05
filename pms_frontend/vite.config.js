45167416b6ca9ed4ef367e6e0196d765f19ad77e
VivekGengadiya <86348334+VivekGengadiya@users.noreply.github.com>
2025-12-04 22:50:21 -0500

    Update vite.config.js

--- pms_frontend/vite.config.js
+++ pms_frontend/vite.config.js
@@ -1,16 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  server: {
+  server: mode === "development" ? {
     proxy: {
-      '/api': {
-        target: 'http://localhost:9000',
+      "/api": {
+        target: "http://localhost:9000",
         changeOrigin: true,
         secure: false,
-      }
-    }
-  }
-})
+      },
+    },
+  } : undefined,
+}));
